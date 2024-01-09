9d1ec8047263a7032c5e487699c88a05fa0d6f89
Saaral_Note <khosoo.fsdev@gmail.com>
2024-01-09 23:22:01 +0800

    gerees fuck

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import "../style/header.css";
 export default function Header() {
   return (
-    <div className="head">
+    <div className="hd">
       <input className="display">0</input>
     </div>
   );
