97514807c7646fc0fa52e5c133d1cab08053c86f
Angi <khsoo.fsdev@gmail.com>
2024-01-10 12:11:01 +0800

    fsfsf

---
+++ src/src/utils/dummyData.js
@@ -0,0 +1,25 @@
+export const DummyProducts = [
+  {
+    prod_name: "Iphone 12",
+    price: 120000,
+    quantity: 12,
+  },
+  {
+    prod_name: "Macbook Pro",
+    price: 220000,
+    quantity: 2,
+  },
+];
+export const DummyNews = [
+  {
+    title: "Khosoo",
+    image: "https://placehold.co/600x400",
+  },
+  {
+    title: "Khosoogiin ergesen tolgoi",
+    image: "https://placehold.co/600x400",
+  },
+];
+
+const MyName = "Andy";
+export default MyName;
